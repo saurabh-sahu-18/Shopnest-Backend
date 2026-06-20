56dc32bde7e9ba13896eca46bac9593762edbec1
Saurabh Sahu <saurabhsahucs@gmail.com>
2026-06-20 23:52:01 +0530

    added authmiddleware & adminMiddleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, getUsers } = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/users", getUsers);
+router.get("/users", protect, admin, getUsers);
 
 module.exports = router;
