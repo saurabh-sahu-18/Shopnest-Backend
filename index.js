2d428a1584a03ac772f04369ea8498610f13780b
Saurabh Sahu <saurabhsahucs@gmail.com>
2026-06-22 23:59:33 +0530

    added express json and env detail

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
 dotenv.config();
 
 connectDb();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("shopnest backend is working properly");
 });
 
 app.use("api/auth", authRoutes);
+app.use("api/products", productRoutes);
+app.use("api/orders", orderRoutes);
+app.use("api/payment", paymentRoutes);
+app.use("api/analytics", analyticsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,20 +2,23 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
 const protect = async (req, res, next) => {
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select("-password");
-            next();
-        } catch (err) {
-            res.status(401).json({ message: "Not authorized, token failed"});
-        }
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Not authorized, token failed" });
     }
-}
-if (!token) {
-    res.status(401).json({message: "Not authorized, No token"});
-}
+  }
 
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, No token" });
+  }
+};
 module.exports = protect;
